b773aa3cf577b4eec9703df7800fbb1fd2315af5
edward <edwardw@qq.com>
2015-11-03 21:10:35 +0800

    ok

--- db.py
+++ db.py
@@ -3,7 +3,7 @@
 # @Author: edward
 # @Date:   2015-11-02 16:51:33
 # @Last Modified by:   edward
-# @Last Modified time: 2015-11-02 22:58:32
+# @Last Modified time: 2015-11-02 23:09:07
 from mydql import connect
 
 
@@ -15,5 +15,5 @@ def init():
     # db.GetField('order_table', 'order_begintime').DateFormat("%H:%i")
     # db.GetField('order_table', 'order_endtime').DateFormat("%H:%i")
     db = connect(host='localhost', db='db',user='root', passwd='123123')
-    db.GetField('student', 'sbirthday').DateFormat("%Y-%m-%d")
+    db.GetField('student', 'sbirthday').DateFormat("%Y-%m-%d", "birth")
     return db
