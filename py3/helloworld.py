738f1a543fc6696b94d3ac4e36ac89de6d9fd77f
edward <edwardw@qq.com>
2015-11-13 10:42:20 +0800

    ok

--- py3/helloworld.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: python
-# @Date:   2015-11-12 15:56:39
-# @Last Modified by:   edward
-# @Last Modified time: 2015-11-12 16:10:06
-print(1)
