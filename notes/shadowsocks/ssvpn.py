4ea086db3636b2ae93134e68372fb6770e699938
edward <edwardw@qq.com>
2016-02-01 23:04:18 +0800

    ok

--- notes/shadowsocks/ssvpn.py
+++ notes/shadowsocks/ssvpn.py
@@ -3,7 +3,7 @@
 # @Author: edward
 # @Date:   2015-12-06 09:42:12
 # @Last Modified by:   edward
-# @Last Modified time: 2016-02-01 22:58:31
+# @Last Modified time: 2016-02-01 23:03:56
 import requests
 from scrapy.selector import Selector
 import os
@@ -19,13 +19,10 @@ port, = Selector(text=vpndetials).re(u'端口：(\d+)')
 #passwd, = Selector(text=vpndetials).re(u'密码：(\d+)')
 passwd, = Selector(text=vpndetials).re(u'码：(\d+)')
 # ip, encrpyt = Selector(text=vpndetials).xpath('//span/text()').extract()
-print vpndetials
 ip, = Selector(text=vpndetials).re(u'服务器地址：([\w\.]+)')
 encrpyt, = Selector(text=vpndetials).re(u'加密方式：([\w-]+)')
 local_port = 1987
 timeout = 600
-print ip
-print encrpyt
 # 
 params = (ip, port, passwd, encrpyt, local_port, sys.argv[1], timeout)
 launch = 'sslocal -s %s -p %s -k %s -m %s -l %s -d %s -t %s' % params
