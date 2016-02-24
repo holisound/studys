ca33383ea667fc0a2de88908e9f0bac641aacfae
edward <edwardw@qq.com>
2016-02-24 09:59:40 +0800

    temp

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -3,22 +3,22 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 
-gulp.task('default', ['watch'], function(){
-  console.log('test');
+gulp.task('default', ['compress', 'watch'], function(){
+  console.log('start...');
 });
 gulp.task('babel', function(){
   return gulp.src('src/*.js')
     .pipe(babel({
 	    presets: ['react', 'es2015']
 	}))
-	.pipe(gulp.dest('/home/python/nginx/static/js/'));
+	.pipe(gulp.dest('build'));
 });
 gulp.task('compress', ['babel'], function(){
-  return gulp.src('bulid/*.js')
+  return gulp.src(['build/*.js', '!build/*demo*'])
     .pipe(uglify())
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(concat('ng-bundle.min.js'))
+    .pipe(gulp.dest('/home/python/nginx/static/js'));
 });
 gulp.task('watch', function() {
-  gulp.watch('src/*.js', ['babel']);
+  gulp.watch('src/*.js', ['compress']);
 });
