3c342bfb5fc55056d573c320d70b60e21105dfd3
edward <edwardw@qq.com>
2016-02-24 18:04:55 +0800

    ok

--- static/src/react-demo-01.js
+++ static/src/react-demo-01.js
@@ -2,7 +2,7 @@
 * @Author: python
 * @Date:   2016-02-24 13:29:23
 * @Last Modified by:   python
-* @Last Modified time: 2016-02-24 15:14:45
+* @Last Modified time: 2016-02-24 18:01:51
 */
 
 'use strict';
@@ -15,25 +15,67 @@ var Toy = React.createClass({
             color: '#fff',
             backgroundColor:'#999',
             width: '100%',
-            height: '30px',
+            height: '35px',
             textAlign: 'center',
             WebkitTransition: 'height .3s ease-out',
                   transition: 'height .3s ease-out',
             },
-            stylesOpen = Object.assign({}, stylesClose);
-            stylesOpen = Object.assign(stylesOpen, {
+            stylesOpen = Object.assign({}, stylesClose, {
                 color: '#000',
                 backgroundColor: '#eee',
                 height: '300px'
             });
 
         return (<div
-                 style={this.state.show ? stylesOpen: stylesClose}
+                 style={this.props.show ? stylesOpen: stylesClose}
                  // onTouchStart={this.handleTouchStart}
-                 onClick={this.handleTouchStart}
-                 >{this.state.show? 'close': 'open'}</div>)
+                 onClick={this.handleTouchStart}>
+                 {/*this.state.show? 'close': 'open'*/}
+                </div>)
     },
     handleTouchStart (e) {
         this.setState({show:!this.state.show})
     }
-});ReactDOM.render(<Toy/>, document.getElementById('example'));
+});
+
+var UploadButton = React.createClass({
+    getInitialState () {
+        return {loaded: false};
+    },
+    render () {
+        let unloadedStyle = {
+                width: '100px',
+                WebkitTransition: 'background-color .3s ease-out',
+                      transition: 'background-color .3s ease-out',
+
+            };
+        return (<button 
+                 style={unloadedStyle}
+                 className={this.state.loaded? 'btn btn-success': 'btn btn-info'}
+                 onClick={this.handleClick}
+                >{this.state.loaded? 'close': 'open'}</button>)
+    },
+    handleClick () {
+        this.props.handler();
+        // this.setState({loaded: !this.state.loaded})
+        this.setState({loaded: !this.props.loaded})
+    }
+});
+var Group = React.createClass({
+    getInitialState () {
+        return {show: false}
+    },
+    handleToggle (){
+        this.setState({ show: !this.state.show});
+        return this.state.show;
+    },
+    render () {
+        return (
+            <div>
+                <Toy show={ this.state.show }/>
+                <UploadButton handler={this.handleToggle} loaded={this.state.show}/>
+            </div>
+        );
+    }
+})
+ReactDOM.render(<Group/>, document.getElementById('example'));
