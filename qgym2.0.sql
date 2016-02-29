29fca163562c37a43a013666dcd12e9e7de23334
edward <edwardw@qq.com>
2016-02-29 11:43:57 +0800

    ok

--- qgym2.0.sql
+++ qgym2.0.sql
@@ -1,11 +1,29 @@
 -- [gallery]
 -- photos grouped and ordered by upload-date desc
-
+-- user can toggle photos' privacy 
 CREATE TABLE IF NOT EXISTS photo_table(
 photo_id INT UNSIGNED KEY AUTO_INCREMENT,
 photo_userid INT UNSIGNED NOT NULL,
-photo_uuid CHAR(64) NOT NULL,
-photo_uploadtime DATETIME DEFAULT NOW(),
-photo_viewtimes INT UNSIGNED DEFAULT 0,
-photo_stars INT UNSIGNED DEFAULT 0
+photo_uuid VARCHAR(64) NOT NULL,
+photo_uploadtime DATETIME NOT NULL DEFAULT NOW(),
+photo_viewtimes INT UNSIGNED NOT NULL DEFAULT 0,
+photo_stars INT UNSIGNED NOT NULL DEFAULT 0,
+photo_privacy INT UNSIGNED NOT NULL DEFAULT 0
+)ENGINE=MyISAM DEFAULT CHARSET=UTF8;
+-- [comment]
+CREATE TABLE IF NOT EXISTS comment_table(
+comment_id INT UNSIGNED KEY AUTO_INCREMENT,
+comment_userid INT UNSIGNED NOT NULL,
+-- comment_type 1: photo
+comment_type INT UNSIGNED  NOT NULL,
+comment_objectid INT UNSIGNED NOT NULL,
+comment_content TEXT NOT NULL,
+comment_postedtime DATETIME NOT NULL DEFAULT NOW()
+)ENGINE=MyISAM DEFAULT CHARSET=UTF8;
+
+CREATE TABLE IF NOT EXISTS relation_table(
+relatioin_id INT UNSIGNED KEY AUTO_INCREMENT,
+relatioin_userid INT UNSIGNED NOT NULL,
+relatioin_private_teacherid INT UNSIGNED NOT NULL,
+relatioin_status INT UNSIGNED NOT NULL
 )ENGINE=MyISAM DEFAULT CHARSET=UTF8;
