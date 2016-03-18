199a7d386f47e288543a39778533d32ad6b44391
edward <edwardw@qq.com>
2016-03-18 17:20:24 +0800

    ok

---
+++ npl01.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+
+def expose(s, initial_length=1, step=1, expand=False, max_length=None):
+    def gen():
+        start, stop = 0, initial_length
+        s_length = len(s)
+        if max_length is None or max_length > s_length:
+            _max_length = s_length
+        t = s[: stop]
+        if expand:
+            while stop <= _max_length:
+                if stop == _max_length:
+                    yield s[-1]
+                    break
+                while stop < _max_length:
+                    yield t
+                    stop += step
+                    t = s[start: stop]
+                start += step
+                stop = start
+        else:
+            while len(t) == initial_length:
+                yield t
+                start += step
+                stop += step
+                t = s[start : stop]
+
+    return gen()
+
+def test():
+    sample01 = u"我的python世界"
+    sample02 = u"我是一个pythoner"
+    word_max_length = 10
+    for i in tuple(expose(sample01, 1, expand=True)):
+        print i
+    for i in tuple(expose(sample02, 1, expand=True)):
+        print i
+
+if __name__ == "__main__":
+    test()
