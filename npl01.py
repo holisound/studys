cfdba8635bec9e9f649d03aa9fc5bd6a385ee6e3
edward <edwardw@qq.com>
2016-03-18 17:43:57 +0800

    ok

--- npl01.py
+++ npl01.py
@@ -2,7 +2,7 @@
 #coding=utf-8
 
 
-def expose(s, initial_length=1, step=1, expand=False, max_length=None):
+def expose(s, initial_length=1, step=1, expand=False, forward=False, max_length=None):
     def gen():
         start, stop = 0, initial_length
         s_length = len(s)
@@ -10,16 +10,18 @@ def expose(s, initial_length=1, step=1, expand=False, max_length=None):
             _max_length = s_length
         t = s[: stop]
         if expand:
-            while stop <= _max_length:
-                if stop == _max_length:
-                    yield s[-1]
-                    break
+            while stop < _max_length:
                 while stop < _max_length:
                     yield t
                     stop += step
                     t = s[start: stop]
-                start += step
-                stop = start
+                if forward:
+                    start += step
+                    stop = start
+            if forward:
+                yield t[-1]
+            else:
+                yield t
         else:
             while len(t) == initial_length:
                 yield t
@@ -33,9 +35,9 @@ def test():
     sample01 = u"我的python世界"
     sample02 = u"我是一个pythoner"
     word_max_length = 10
-    for i in tuple(expose(sample01, 1, expand=True)):
+    for i in tuple(expose(sample01, 1, forward=True, expand=True)):
         print i
-    for i in tuple(expose(sample02, 1, expand=True)):
+    for i in tuple(expose(sample02, 1, expand=False)):
         print i
 
 if __name__ == "__main__":
