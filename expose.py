1881fab7dacb2243c78038a07e6ac2e0089878c0
edward <edwardw@qq.com>
2016-03-19 15:05:29 +0800

    ok

--- expose.py
+++ expose.py
@@ -32,9 +32,9 @@ def test():
     sample01 = u"我的python世界"
     sample02 = u"我是一个pythoner"
     word_max_length = 10
-    for i in tuple(expose(sample01, 1, expand=True)):
+    for i in expose(sample01, 1, expand=True):
         print i
-    for i in tuple(expose(sample02, 2,2, expand=False)):
+    for i in expose(sample02, 2,2, expand=False):
         print i
 
 if __name__ == "__main__":
