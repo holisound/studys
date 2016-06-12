92c146ba31584d41f5a053a3a1d2778b12b125d0
edward <edwardw@qq.com>
2016-06-12 18:04:28 +0800

    ok

--- snippets.py
+++ snippets.py
@@ -1,18 +1,13 @@
 def valid_arguments(self):
 	r = {}
 	for k, v in self.request.arguments.items():
-	    if k.startswith('_'): continue
-	    v = [ i for i in v if len(i) > 0]
-	    if len(v) == 1:
-	        tv = v[0]
-	    else:
-	        tv = ','.join(v)
-	    r[k] = tv
+		if k.startswith('_'): continue
+		v = [ i for i in v if len(i) > 0]
+		if len(v) == 0:
+			continue
+		elif len(v) == 1:
+			tv = v[0]
+		else:
+			tv = ','.join(v)
+		r[k] = tv
 	return r
-															              v = [ i for i in v if len(i) > 0]
-																					              if len(v) == 1:
-																										                tv = v
-																													               else:
-																																                tv = ','.join(v)
-																																		             r[k] = tv
-																																			           return r
