df544dbf2a49f6d519ceb694a255fd1244a3d645
Edward Wang <wenhua.wang@tcs.com>
2022-03-02 22:22:57 +0800

    Update

--- test_crop_img.py
+++ test_crop_img.py
@@ -1,6 +1,5 @@
 import numpy as np
 from PIL import Image
-import time
 
 RED_UPPER = np.array([255, 127, 80])
 RED_LOWER = np.array([156, 0, 0])
@@ -32,20 +31,18 @@ def main(src, dst):
 def check_neighbors(mat, r, c, R, C):
     if not in_range(mat[r][c]):
         return False
-    left_top = []
-    right_bottom = []
+    top_left = True
+    bottom_right = True
     for i in range(1 << 4):
         if c + i < C:
-            left_top.append((r, c + i))
+            top_left &= in_range(mat[r][c + i])
         if r + i < R:
-            left_top.append((r + i, c))
+            top_left &= in_range(mat[r + i][c])
         if r - i > -1:
-            right_bottom.append((r - i, c))
+            bottom_right &= in_range(mat[r - i][c])
         if c - i > -1:
-            right_bottom.append((r, c - i))
-    is_left_top = all(in_range(mat[rx][cx]) for rx, cx in left_top)
-    is_right_bottom = all(in_range(mat[rx][cx]) for rx, cx in right_bottom)
-    return is_left_top or is_right_bottom
+            bottom_right &= in_range(mat[r][c - i])
+    return top_left or bottom_right
 
 
 if __name__ == '__main__':
