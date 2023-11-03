e64cd0fdad007bdd6b63bc8c832a2acad86add5a
Edward Wang <edwardw@qq.com>
2023-11-03 17:58:59 +0800

    Update weightchoice.py

--- weightchoice.py
+++ weightchoice.py
@@ -6,11 +6,12 @@
 # @Last Modified time: 2016-06-08 09:44:23
 import random
 
+
 class T:
     def WeightedChoice(self, choices):
-        '''加权随机
-            choices: 参数列表，格式：[("iPhone 6 Plus", 1), ("A box of Chocolate", 10), ("A Pencil", 33), ("Free Course of Training", 10), ("iPod Touch", 6), ("Thank you!", 40)]
-        '''
+        """加权随机
+        choices: 参数列表，格式：[("iPhone 6 Plus", 1), ("A box of Chocolate", 10), ("A Pencil", 33), ("Free Course of Training", 10), ("iPod Touch", 6), ("Thank you!", 40)]
+        """
         total = sum(w for c, w in choices)
         r = random.uniform(0, total)
         upto = 0
@@ -27,11 +28,25 @@ class T:
             ret = self.WeightedChoice(price_pool)
             if ret == price_name:
                 total += 1
-        print "The %s probability is: %.2f%%" % (price_name, float(float(total) / float(maxcount)) * 100)
+        text = "The %s probability is: %.2f%%" % (
+            price_name,
+            float(float(total) / float(maxcount)) * 100,
+        )
+        print(text)
+
 
 def main():
-    PRIZE = [("iPhone 6 Plus", 100), ("A box of Chocolate", 1000), ("A Pencil", 3300), ("Free Course of Training", 1000), ("iPod Touch", 600), ("Thank you!", 4000)]
+    PRIZE = [
+        ("iPhone 6 Plus", 100),
+        ("A box of Chocolate", 1000),
+        ("A Pencil", 3300),
+        ("Free Course of Training", 1000),
+        ("iPod Touch", 600),
+        ("Thank you!", 4000),
+    ]
     t = T()
     t.TestWeightedChoice(PRIZE, PRIZE[0][0])
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
